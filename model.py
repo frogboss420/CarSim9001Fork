12817d2a7b32ee46f9ef5e511c7497a9f9cd1903
frogboss420 <mads.friis.nielsen@gmail.com>
2025-10-29 09:57:35 +0100

    Kommentarer

--- model.py
+++ model.py
@@ -1,18 +1,23 @@
 from random import randint
 
+
 class Car(object):
     def __init__(self):
         self.theEngine = Engine()
     def updateModel(self,dt):
         self.theEngine.updateModel(dt)
 
+
 class Wheel(object):
     def __init__(self):
         self.orientation: int = randint(0,360)
+
+#Modtager argumentet "revolutions" for at tillade en vilkårlig rotation, afhængende af omdrejninger i Engine.
     def rotate(self,revolutions):
         revs_in_degrees = 360 * revolutions
         self.orientation += revs_in_degrees
-        self.orientation %= 360
+        self.orientation %= 360 #sikrer at hjulets orientation ikke overstiger 360
+
 
 class Engine(object):
     def __init__(self):
@@ -23,6 +28,7 @@ class Engine(object):
         self.maxRpm: int = 100
         self.theTank = Tank()
 
+
     def updateModel(self,dt):
         self.currentRpm = self.throttlePosition * self.maxRpm
         self.theTank.remove(self.currentRpm * self.consumptionConstant)
@@ -32,27 +38,31 @@ class Engine(object):
 
 
 class Gearbox(object):
+
+
     def __init__(self):
         self.wheels: dict = {'frontLeft': Wheel(), 'frontRight': Wheel(), 'rearLeft': Wheel(), 'rearRight': Wheel()}
         self.currentGear: int = 0
         self.clutchEngaged: bool = False
-        self.gears: list = [0, 0.8, 1, 1.4, 2.2, 3.8]
+        self.gears: list = [0, 0.8, 1, 1.4, 2.2, 3.8] #indeks svarer til hvilket gear bilen er i, elementerne er koefficienten til omdrejninger.
+
 
     def shiftUp(self):
-        if self.clutchEngaged == False and self.currentGear != len(self.gears) - 1:
+        if self.clutchEngaged == False and self.currentGear != len(self.gears) - 1: #sikrer at koblingen ikke er koblet, og at nuværende gear ikke allerede er det højeste.
             self.currentGear += 1
         else: pass
 
+
     def shiftDown(self):
-        if self.clutchEngaged == False and self.currentGear != 0:
+        if self.clutchEngaged == False and self.currentGear != 0: #sikrer at koblingen ikke er koblet, og at nuværende gear ikke allerede er det laveste.
             self.currentGear -= 1
         else: pass
 
+#Ændrer orientationen af hver instansieret wheel afhængigt af hvilket gear bilen er i.
     def rotate(self,revolutions):
         if self.clutchEngaged:
             for wheel in self.wheels.values():
                 wheel.rotate(revolutions * self.gears[self.currentGear])
-
 
 
 class Tank(object):
